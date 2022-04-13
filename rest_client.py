1eedd28a69b46d77ff659b19d8a95c13f135b495
IuliaMihaela <20IMC10290@fh-krems.ac.at>
2022-04-13 23:50:59 +0300

    master data service: jobs

--- rest_client.py
+++ rest_client.py
@@ -1,7 +1,14 @@
 from requests import put, get, post
 
-r = post('http://localhost:5000/users/registration/api/felicity', json={'password':"felicity12", 'role':'manager'})
-print(r.json())
+# r = post('http://localhost:5000/users/registration/api/felicity', json={'password':"felicity12", 'role':'manager'})
+# print(r.json())
+#
+# r = put('http://localhost:5000/users/login/api', json={'username':'felicity', 'password':"felicity12"})
+# print(r.json())
 
-r = put('http://localhost:5000/users/login/api', json={'username':'felicity', 'password':"felicity12"})
-print(r.json())
+r = post('http://localhost:5000/jobs/api', json={'username': 'felicity', 'token': "manager-87ef903f-bb63-11ec-9a15-44af28c6779f", "timestamp": "13-04-2022 22:51:12", "date_range":"13-15", "assets":[1,2,3,4,5,11]})
+
+
+
+
+
--- rest_server.py
+++ rest_server.py
@@ -5,10 +5,10 @@ import uuid
 app = Flask(__name__)
 api = Api(app)
 
-users = {}
 
-def authentication(username, password):
-    pass
+# Authentication Service
+
+users = {}
 
 def validate_registration(data):
     # if "username" not in data:
@@ -29,7 +29,7 @@ def validate_login(data):
     return ""
 
 def emit_token(data):
-    return users[data["username"]]["role"]+str(uuid.uuid1())
+    return users[data["username"]]["role"]+'-'+str(uuid.uuid1())
 
 class Registration(Resource):
     def post(self, username):
@@ -64,19 +64,146 @@ def verify_token(username,token):
     return {"success": True }
 
 
-
-
 api.add_resource(Registration, '/users/registration/api/<string:username>')
 api.add_resource(Login, '/users/login/api')
 
-class User(Resource):
+
+
+# Master Data Service
+
+# jobs ={id:{
+#         user:""
+#         timestamp:""
+#         status:""
+#         date_range:""
+#         assets:""}
+#        }
+
+jobs = {}
+
+def validate_post_job(data):
+    if "username" not in data:
+        return {"error": "no user provided"}
+    if "token" not in data:
+        return {"error": "no token provided"}
+    if "timestamp" not in data:
+        return {"error": "no timestamp provided"}
+    if "status" not in data:
+        return {"error": "status not provided"}
+    if "date_range" not in data:
+        return {"error": "date_range not provided"}
+    if "assets" not in data:
+        return {"error": "assets not provided"}
+    return ""
+
+def validate_get_job(data):
+    if "username" not in data:
+        return {"error": "no user provided"}
+    if "token" not in data:
+        return {"error": "no token provided"}
+    if "job_id" not in data:
+        return {"error": "no job_id provided"}
+    return ""
+
+def validate_update_job(data):
+    if "job_id" not in data:
+        return {"error": "no job_id provided"}
+    if "username" not in data:
+        return {"error": "no user provided"}
+    if "token" not in data:
+        return {"error": "no token provided"}
+    if "timestamp" not in data:
+        return {"error": "no timestamp provided"}
+    if "status" not in data:
+        return {"error": "status not provided"}
+    if "date_range" not in data:
+        return {"error": "date_range not provided"}
+    if "assets" not in data:
+        return {"error": "assets not provided"}
+    return ""
+
+def validate_user_permission_master_data(username, token):
+    token_state = verify_token(username, token)
+    if token_state['success'] == True:
+        return {"error": "Token not valid"}
+    if username not in users.keys() or token.split('-')[0] == "secretary":
+        return {"error": "Authorization Error"}
+    return ""
+
+
+def create_job(data):
+    new_job_id = str(uuid.uuid1())
+    del data["token"]
+    new_job = {new_job_id:data}
+    jobs.update(new_job)
+    return new_job
+
+# def submit_job(job):
+#     jobs.update(job)
+#     return job
+
+def fetch_job(job_id):
+    return jobs[job_id]
+
+def update_job(data):
+    job_data = jobs[data["id"]]
+    id=data['id']
+    del data["id"]
+    jobs[id] = job_data
+    return jobs[id]
+
+class Jobs(Resource):
+    def post(self):
+        validation = validate_post_job(request.json)
+        if validation != "":
+            return validation
+        data = request.json
+        validation = validate_user_permission_master_data(data["username"], data["token"])
+        if validation != "":
+            return validation
+
+        new_job = create_job(request.json)
+        return new_job
+
+    def get(self):
+        validation = validate_get_job(request.json)
+        if validation != "":
+            return validation
+        data = request.json
+        validation = validate_user_permission_master_data(data["username"],data["token"])
+        if validation != "":
+            return validation
+        job = fetch_job(data["id"])
+        return job
+
+    def put(self):
+        validation = validate_update_job(request.json)
+        if validation != "":
+            return validation
+        data = request.json
+        validation = validate_user_permission_master_data(data["username"], data["token"])
+        if validation != "":
+            return validation
+        updated_job = update_job(request.json)
+        return updated_job
+
+class Results(Resource):
     pass
 
-#str(uuid.uuid1())
+
+api.add_resource(Jobs, '/jobs/api')
+api.add_resource(Results, '/results/api')
 
 
+##### NOTES ####
+# only token as in-memory cash, the rest of user data as persistent storage ?
+# persistent storage: pickle, json, sql lite ?
+# tokens expire ?
+# separate resources ?
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
