33b16d7b4d669441145fc251d9830aec3aa402db
Felicity Olivia Ogoke <79579227+misprout@users.noreply.github.com>
2022-05-15 20:17:53 +0200

    Update message_queues_service.py

--- services/message_queues_service.py
+++ services/message_queues_service.py
@@ -9,7 +9,7 @@ import uuid
 
 
 queue_results = queue.Queue()
-queue_jobs = queue.Queue()
+
 
 
 def validate_user_permission_append_pull_push(username, token):
@@ -44,12 +44,12 @@ def validate_push_job(data):
         return {"error": "no job_id provided"}
     return ""
 
+
 class handling_job_queues(Resource):
     def __init__(self, list_of_queues):
         self.list_of_queues = []
 
-    def create(self, queue):
-
+    def create(self, queue_name):
         data = request.json
         validation = validate_user_permission_delete_create(data["username"],
                                                             data["token"])  # check if the user has permission
@@ -57,35 +57,41 @@ class handling_job_queues(Resource):
             return validation
 
         # check if queue already exists
-
-        if queue in self.list_of_queues:
-            return {"error": "queue " + queue + " already exists"}
+        if queue_name in self.list_of_queues:
+            return {"error": "queue " + queue_name + " already exists"}
         else:
-            queue = queue.Queue()
-            self.list_of_queues.append(queue)
+            queue_name = queue.Queue()
+            self.list_of_queues.append(queue_name)
 
 
 
-    def delete(self,queue):
+    def delete(self,queue_name):
         data = request.json
         validation = validate_user_permission_delete_create(data["username"],
                                                             data["token"])  # check if the user has permission
         if validation != "":
             return validation
 
+        #if queue not in list
+        if queue_name not in self.list_of_queues:
+            return {"error": "queue " + queue_name + " not in list"}
+
         # deleting messages in queue
-        with queue_jobs.mutex:
-            queue_jobs.queue.clear()
+        with queue_name.mutex:
+            queue_name.queue.clear()
 
         # deleting queue from list
-        self.list_of_queues.remove(queue)
+        self.list_of_queues.remove(queue_name)
 
     def Listing(self):
         data = request.json
-        
+
         # listing queue
         return self.list_of_queues
 
+
+queue_jobs = handling_job_queues.create("queue_jobs")
+print(queue_jobs.queue)
 
 class Jobs_Queue(Resource):
 
@@ -113,30 +119,69 @@ class Jobs_Queue(Resource):
         if queue_jobs.empty() == True:
             return {"error": "job queue is empty"}
 
+
         job_pulled = queue_jobs.get()
+
+        # push into the results queue
+        queue_results.put(job_pulled)
+
+
         return "pulled ", job_pulled
+
+
+
+
+
 
         # have to send contents to caller
 
 
 
 
+###################results#############################
+
+def validate_result(data):
+    # checking if all data needed for posting or updating a result was provided by the client
+    if "username" not in data:
+        return {"error": "no user provided"}
+    if "token" not in data:
+        return {"error": "no token provided"}
+    if "job_id" not in data:
+        return {"error": "job_id not provided"}
+    if "timestamp" not in data:
+        return {"error": "no timestamp provided"}
+    if "assets" not in data:
+        return {"error": "assets not provided"}
+    return ""
+
 
 class Results_Queue(Resource):
-    def post(self):
-        pass
 
+    def push(self):
+        validation = validate_result(request.json)  # check if all data was provided
+        if validation != "":
+            return validation
 
+        data = request.json
+        validation = validate_user_permission_append_pull_push(data["username"],
+                                                               data["token"])  # check if the user has permission
+        if validation != "":
+            return validation
 
+        result = fetch_result(data["job_id"])
+        queue_results.put(result)
 
+    def pull(self):
+        data = request.json
+        validation = validate_user_permission_append_pull_push(data["username"],
+                                                               data["token"])  # check if the user has permission
+        if validation != "":
+            return validation
 
+        # check if queue is not empty
+        if queue_results.empty() == True:
+            return {"error": "results queue is empty"}
 
+        result_pulled = queue_results.get()
 
-
-
-
-
-
-
-
-
+        return "pulled", result_pulled
